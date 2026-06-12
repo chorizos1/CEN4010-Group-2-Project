e8e97a891a2ac84d35a2e5cd3473ab1431122d16
chorizos1 <nickolai004@gmail.com>
2026-06-12 10:46:48 -0400

    test

---
+++ myproject/myproject/views.py
@@ -0,0 +1,11 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+import supabase
+
+def homepage(req):
+    # return HttpResponse("Hello World!")
+    return render(req, 'home.html')
+
+def aboutpage(req):
+    # return HttpResponse("About page.")
+    return render(req, 'about.html')
