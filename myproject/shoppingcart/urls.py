800ee784f0cc689f91eb66d0eb10a6f1fd430fa3
chorizos1 <nickolai004@gmail.com>
2026-07-23 08:54:08 -0400

    added remaining missing feature checklist features

--- myproject/shoppingcart/urls.py
+++ myproject/shoppingcart/urls.py
@@ -3,6 +3,10 @@ from . import views
 
 urlpatterns = [
     path('', views.cart),
-    path('get-cart/', views.get_cart_view, name='get_shopping_cart')
-    
+    path('get-cart/', views.get_cart_view, name='get_shopping_cart'),
+    path('remove-from-cart/', views.remove_from_cart, name = 'remove_from_cart'),
+    path('remove-cart/', views.removeCart, name = "removeCart"),
+    path('create-cart/', views.createCart, name = "createCart"),
+    path('get-all-books/', views.getAllBooksAvailable, name = "getAllBooks"),
+    path('add-book/', views.addBookToCart, name = "addBook")
 ]
--- myproject/shoppingcart/views.py
+++ myproject/shoppingcart/views.py
@@ -6,13 +6,14 @@ import os
 from django.http import JsonResponse
 from supabase import create_client, Client
 from django.shortcuts import render
+from django.views.decorators.csrf import csrf_exempt
 
 # Supabase URL, key, and client
 SUPABASE_URL = os.environ.get("SUPABASE_URL")
 SUPABASE_KEY = os.environ.get("SUPABASE_KEY")
 SupabaseDB = create_client(SUPABASE_URL, SUPABASE_KEY)
 
-# Create your views here.
+@csrf_exempt
 def get_cart_view(req):
 
     if(req.method == 'POST'):
@@ -23,13 +24,14 @@ def get_cart_view(req):
             email = data.get('email')
             password = data.get('password')
 
-            
             session = SupabaseDB.auth.sign_in_with_password({
 
                 "email": email,
                 "password": password,
 
             })
+
+            SupabaseDB.postgrest.auth(session.session.access_token)
 
             accountID = SupabaseDB.table("Account").select("id").eq("email", email).execute()
             
@@ -39,9 +41,19 @@ def get_cart_view(req):
             
             else:
                 return JsonResponse({"error": f'No shopping cart found for user "{email}".'}, status=404)
-            
+
+            if(not shoppingCartItems): 
+
+                return JsonResponse({
+
+                    "error": f'No shopping cart found for user "{email}".',
+                    "cartRowEmpty": True,
+
+                }, status = 404)
+
             shoppingCartString = f"Shopping cart items for {email}:\n\n"
-            shoppingCartDictionary = {}
+            shoppingCartData = []
+            totalCartPrice = 0
 
             # Gets all book IDs in the from the cart column in the Shopping_Cart table.
             for item in shoppingCartItems[0]["cart"]:
@@ -49,27 +61,29 @@ def get_cart_view(req):
                 dbDataResponse = SupabaseDB.table('Books').select('*').eq('bookID', value=item).execute()
                 bookInformation = dbDataResponse.data
 
-                # Stores relevant book data into a dictionary with its respective ID as the key.
-                shoppingCartDictionary[item] = {
+                shoppingCartData.append(
 
-                    "bookName": bookInformation[0]["bookName"],
-                    "bookAuthor": bookInformation[0]["bookAuthor"],
-                    "bookDescription": bookInformation[0]["bookDescription"],
-                    "bookPrice": bookInformation[0]["price"],
-                    "bookISBN13": bookInformation[0]["ISBN13"],
-                    "bookPublishDate": bookInformation[0]["publishedDate"],
+                    {
+                        "bookID": bookInformation[0]["bookID"],
+                        "bookName": bookInformation[0]["bookName"],
+                        "Author": bookInformation[0]["bookAuthor"],
+                        "Description": bookInformation[0]["bookDescription"],
+                        "Published": bookInformation[0]["publishedDate"],
+                        "ISBN13": bookInformation[0]["ISBN13"],
+                        "Price": bookInformation[0]["price"],
+                    }
 
-                }
+                )
 
-                # Constructs the string output used when displaying the shopping cart.
-                shoppingCartString = shoppingCartString + f"- {shoppingCartDictionary[item]["bookName"]}\n"
-                shoppingCartString = shoppingCartString + f"Author: {shoppingCartDictionary[item]["bookAuthor"]}\n"
-                shoppingCartString = shoppingCartString + f"Description: {shoppingCartDictionary[item]["bookDescription"]}\n"
-                shoppingCartString = shoppingCartString + f"Published: {shoppingCartDictionary[item]["bookPublishDate"]}\n"
-                shoppingCartString = shoppingCartString + f"ISBN13: {shoppingCartDictionary[item]["bookISBN13"]}\n"
-                shoppingCartString = shoppingCartString + f"Price: ${shoppingCartDictionary[item]["bookPrice"]}\n\n"
+                totalCartPrice = totalCartPrice + bookInformation[0]["price"]
 
-            return JsonResponse({"Cart_data": shoppingCartString})
+            return JsonResponse({
+
+                "Cart_msg": shoppingCartString,
+                "Cart_data": shoppingCartData,
+                "Total_cart_price": totalCartPrice,
+
+            })
 
         except Exception as err:
 
@@ -77,6 +91,201 @@ def get_cart_view(req):
         
     return JsonResponse({"error": "Invalid request method"}, status=400)
 
+@csrf_exempt
+def remove_from_cart(req):
+
+    if(req.method == 'PUT'):
+
+        try:
+
+            data = json.loads(req.body)
+            email = data.get('email')
+            password = data.get('password')
+            bookID = int(data.get('bookID'))
+
+            session = SupabaseDB.auth.sign_in_with_password({
+            
+                "email": email,
+                "password": password,
+            
+            })
+
+            SupabaseDB.postgrest.auth(session.session.access_token)
+            
+            accountID = SupabaseDB.table("Account").select("id").eq("email", email).execute()
+            shoppingCart = []
+
+            if(accountID.data):
+                dbDataResponse = SupabaseDB.table('Shopping_Cart').select('cart').eq('customerUUID', value=accountID.data[0]["id"]).execute()
+                shoppingCart = dbDataResponse.data[0]["cart"]
+                        
+            else:
+                return JsonResponse({"error": f'No shopping cart found for user "{email}".'}, status=404)
+
+            shoppingCart.remove(bookID)
+            SupabaseDB.table("Shopping_Cart").update({"cart": shoppingCart}).eq("customerUUID", accountID.data[0]["id"]).execute()
+
+            return JsonResponse({"success": True})
+
+        except Exception as err:
+
+            return JsonResponse({"error": str(err)}, status = 500)
+
+    return JsonResponse({"error": "Invalid request method"}, status=400)
+
+@csrf_exempt
+def removeCart(req):
+
+    if(req.method == 'DELETE'):
+
+        try:
+
+            data = json.loads(req.body)
+            email = data.get('email')
+            password = data.get('password')
+
+            session = SupabaseDB.auth.sign_in_with_password({
+                                
+                "email": email,
+                "password": password,
+                                
+            })
+                    
+            SupabaseDB.postgrest.auth(session.session.access_token)
+                                
+            accountID = SupabaseDB.table("Account").select("id").eq("email", email).execute()
+            
+            if(accountID.data):
+            
+                SupabaseDB.table("Shopping_Cart").delete().eq("customerUUID", accountID.data[0]["id"]).execute()
+            
+            else:
+            
+                return JsonResponse({"error": f'No shopping cart found for user "{email}".'}, status=404)
+            
+            return JsonResponse({"success": True})
+
+        except Exception as err:
+
+            return JsonResponse({"error": str(err)}, status = 500)
+
+    return JsonResponse({"error": "Invalid request method"}, status=400)
+
+@csrf_exempt
+def createCart(req):
+
+    if(req.method == "POST"):
+
+        try:
+
+            data = json.loads(req.body)
+            email = data.get('email')
+            password = data.get('password')
+            
+            session = SupabaseDB.auth.sign_in_with_password({
+                                            
+                "email": email,
+                "password": password,
+                                            
+            })
+                                
+            SupabaseDB.postgrest.auth(session.session.access_token)
+                                            
+            accountID = SupabaseDB.table("Account").select("id").eq("email", email).execute()
+
+            createdCart = SupabaseDB.table("Shopping_Cart").insert({
+
+                "customerUUID": accountID.data[0]["id"],
+                "cart": [1, 4, 8],
+
+            }).select().execute()
+
+            if(createdCart.data):
+
+                return JsonResponse({"success": True}, status = 200)
+
+            else:
+
+                return JsonResponse({"error": f'Cart was unable to be created. Try again later.'}, status = 500)
+
+        except Exception as err:
+
+            return JsonResponse({"error": str(err)}, status = 500)
+
+def getAllBooksAvailable(req):
+
+    if(req.method == 'GET'):
+
+        try:
+            
+            allBooks = SupabaseDB.table("Books").select("*").execute()
+            reformattedBooksVar = []
+
+            for bookInfo in allBooks.data:
+
+                reformattedBooksVar.append({
+
+                    "bookID": bookInfo["bookID"],
+                    "bookName": bookInfo["bookName"],
+                    "bookAuthor": bookInfo["bookAuthor"],
+
+                })
+
+            return JsonResponse({
+
+                "success": True,
+                "booksData": reformattedBooksVar,
+
+            }, status = 200)
+
+        except Exception as err:
+
+            return JsonResponse({"error": str(err)}, status = 500)
+
+    return JsonResponse({"error": "Invalid request method"}, status=400)
+
+def addBookToCart(req):
+
+    if(req.method == 'POST'):
+
+        try:
+
+            data = json.loads(req.body)
+            email = data.get('email')
+            password = data.get('password')
+            bookID = data.get('bookID')
+                        
+            session = SupabaseDB.auth.sign_in_with_password({
+                                                        
+                "email": email,
+                "password": password,
+                                                        
+            })
+                                            
+            SupabaseDB.postgrest.auth(session.session.access_token)
+                                                        
+            accountID = SupabaseDB.table("Account").select("id").eq("email", email).execute()
+            shoppingCart = []
+
+            if(accountID.data):
+                dbDataResponse = SupabaseDB.table('Shopping_Cart').select('cart').eq('customerUUID', value=accountID.data[0]["id"]).execute()
+                shoppingCart = dbDataResponse.data[0]["cart"]
+                                    
+            else:
+                return JsonResponse({"error": f'No shopping cart found for user "{email}".'}, status=404)
+            
+            shoppingCart.append(bookID)
+            SupabaseDB.table("Shopping_Cart").update({"cart": shoppingCart}).eq("customerUUID", accountID.data[0]["id"]).execute()
+            
+            return JsonResponse({"success": True})
+
+        except Exception as err:
+
+            return JsonResponse({"error": str(err)}, status = 500)
+
+    return JsonResponse({"error": "Invalid request method"}, status=400)
+
+@csrf_exempt
 def cart(req):
     return render(req, 'shoppingcart/shoppingcart.html')
 
